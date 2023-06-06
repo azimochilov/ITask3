1c6596017aae4e4fa62b251444746a075b956513
azimochilov <azimochilov226@gmail.com>
2023-06-06 10:27:34 +0300

    winnerchecker implemented

---
+++ WinnerChecker.cs
@@ -0,0 +1,17 @@
+﻿namespace Task3;
+
+public class WinnerChecker
+{
+    public int CheckWinner(int player1Move, int player2Move, string[] choices)
+    {
+        int halfChoicesCount = choices.Length / 2;
+
+        if (player1Move == player2Move)
+            return 0;
+        else if (player2Move > player1Move && player2Move - player1Move <= halfChoicesCount
+            || player2Move < player1Move && player2Move <= player1Move - halfChoicesCount - 1)
+            return 1;
+        else
+            return 2;
+    }
+}
